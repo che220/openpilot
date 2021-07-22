dba69719f031ef3217a15b168961e9e0c547426d
Igor Biletksyy <bs@privacy.im>
2021-07-22 13:06:35 -0700

    dos silencer

---
+++ silence_dos/pandad.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# simple boardd wrapper that updates the panda first
+import os
+
+from panda import Panda
+from common.basedir import BASEDIR
+
+PANDA_FW_FN = os.path.join(BASEDIR, "silence_dos", "silent_dos_panda.bin.signed")
+
+
+def main() -> None:
+  panda_list = Panda.list()
+    if len(panda_list) > 0:
+      for tmp_panda in panda_list:
+        panda = Panda(tmp_panda)
+        if tmp_panda.is_dos():
+          tmp_panda.flash(fn=PANDA_FW_FN)
+
+
+if __name__ == "__main__":
+  main()
