57e646fb87fc8ddbc64b26e18672be6fa60eed10
Igor Biletksyy <bs@privacy.im>
2021-07-22 14:14:49 -0700

    typo

--- silence_dos/pandad.py
+++ silence_dos/pandad.py
@@ -10,11 +10,11 @@ PANDA_FW_FN = os.path.join(BASEDIR, "silence_dos", "silent_dos_panda.bin.signed"
 
 def main() -> None:
   panda_list = Panda.list()
-    if len(panda_list) > 0:
-      for tmp_panda in panda_list:
-        panda = Panda(tmp_panda)
-        if tmp_panda.is_dos():
-          tmp_panda.flash(fn=PANDA_FW_FN)
+  if len(panda_list) > 0:
+    for tmp_panda in panda_list:
+      panda = Panda(tmp_panda)
+      if tmp_panda.is_dos():
+        tmp_panda.flash(fn=PANDA_FW_FN)
 
 
 if __name__ == "__main__":
